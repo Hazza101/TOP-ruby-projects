f8d6f2266f00aa3e437c42ee0159b81635848f67
harri <harri.bain@gmail.com>
2026-08-01 13:26:54 +0100

    Implement basic caesar cipher

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,1 +1,28 @@
-puts 'Hello World'
+require 'pry-byebug'
+DOWNCASE_LOWER = 'a'.ord
+DOWNCASE_UPPER = 'z'.ord
+UPCASE_LOWER = 'A'.ord
+UPCASE_UPPER = 'Z'.ord
+
+def shift_letter(char, shift)
+  code = char.ord
+  return char if !code.between?(DOWNCASE_LOWER, DOWNCASE_UPPER) && !code.between?(UPCASE_LOWER, UPCASE_UPPER)
+
+  diff = 0
+  diff = if code.between?(DOWNCASE_LOWER, DOWNCASE_UPPER)
+           DOWNCASE_LOWER
+         else
+           UPCASE_LOWER
+         end
+  new_code = ((code - diff) + shift) % 26 + diff
+  new_code.chr
+end
+
+def caesar_cipher(string, shift)
+  string.split('').map do |char|
+    shift_letter(char, shift)
+  end
+end
+binding.pry
+result = caesar_cipher('Hello World!', 5)
+puts result
