696d86b89a571b4c7fc150e6f51a1a96cfb74094
Harri Bain <harri.bain@gmail.com>
2026-09-07 22:26:14 +0100

    Add random player class

--- tic_tac_toe/main.rb
+++ tic_tac_toe/main.rb
@@ -8,14 +8,27 @@ class HumanPlayer
   end
 end
 
+class RandomPlayer
+  def make_move(board)
+    arr = Array.new 
+    (0..2).each do |i|
+      (0..2).each do |j|
+        arr.append([i, j]) if board[i][j] == PLAYERS::NONE
+      end
+    end
+    index = rand(arr.size)
+    arr[index]
+  end
+end
+
 def play_game?(board, _player1, _player2)
   res = board.check_winner?
   while res == PLAYERS::NONE
     Printing.display_board(board.board)
     if board.turn == PLAYERS::PLAYER_ONE
-      i, j = _player1.make_move(board)
+      i, j = _player1.make_move(board.board)
     else
-      i, j = _player2.make_move(board)
+      i, j = _player2.make_move(board.board)
     end
     is_valid_move = board.make_move(i, j)
     unless is_valid_move
@@ -30,8 +43,8 @@ end
 
 def main_loop
   board = Board.new
-  player1 = HumanPlayer.new
-  player2 = HumanPlayer.new
+  player1 = RandomPlayer.new
+  player2 = RandomPlayer.new
   result = play_game?(board, player1, player2)
   puts "Player 1 (x) won" if result == PLAYERS::PLAYER_ONE
   puts "Player 2 (o) won" if result == PLAYERS::PLAYER_TWO
